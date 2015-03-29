ff086c18a5558bf94f9df88be74e5dfeaba78d40
Hellon Canella Machado <helloncanella@gmail.com>
2015-03-29 19:48:31 -0300

    First commit of day

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150326144211) do
+ActiveRecord::Schema.define(version: 20150328222258) do
 
   create_table "products", force: true do |t|
     t.text    "name"
     t.text    "description"
     t.decimal "preco"
     t.string  "image"
+  end
+
+  create_table "testes", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "tests", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
